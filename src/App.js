3082691cb22581cecb8e8dc4e2e93dc6f8d9c5d1
Chris Chateau <christopherchateau@gmail.com>
2018-09-04 19:37:23 -0600

    create components dir, setup files. clean up index.html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './mockAPI';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Search from "./Components/Search";
+import Card from "./Components/Card";
+import CurrentWeather from "./Components/CurrentWeather";
+import SevenHour from "./Components/SevenHour";
+import TenDay from "./Components/TenDay";
+import Welcome from "./Components/Welcome";
+import "./mockAPI";
 
 class App extends Component {
   render() {
