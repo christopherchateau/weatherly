cf9dc1a9e0e9d1ff49aa9692e44c767370ff8f9a
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-09-05 14:59:25 -0600

    Add card and sevenhour component functionality

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Card extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() { 
-        return ( <div></div> );
-    }
+function Card(props) {
+    console.log(props)
+    return(
+      <div>
+      
+      <h1>{props.data}</h1> 
+      </div> 
+  )
 }
 
 export default Card;
+
+// the hour
+// an image that reflects the weather
+// the projected temperature
+
+//Line 1062 in mock data Hourly forecast 
+
