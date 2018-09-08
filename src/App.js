601bd5047c2140b16e8c885cbf586824bec8e704
Chris Chateau <christopherchateau@gmail.com>
2018-09-08 14:50:23 -0600

    Add apiKey file, add file path to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import React, { Component } from "react";
-
 import Search from "./Components/Search";
 import CurrentWeather from "./Components/CurrentWeather";
 import SevenHour from "./Components/SevenHour";
 import TenDay from "./Components/TenDay";
 import Welcome from "./Components/Welcome";
 import data from "./mockAPI.js";
-// import ToggleWeather from './Components/ToggleWeather'
+import apiConfig from "./apiKey";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
+    console.log(apiConfig.weatherUndergroundKey)
+
     this.state = {
       city: data.current_observation.display_location.full,
       currentObservation: data.current_observation,
       hourlyForecast: data.hourly_forecast,
       dailyForecast: data.forecast,
-      isToggleOn: true 
+      isToggleOn: true
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   getWeather = () => {
-    this.setState({
-      
-    });
+    this.setState({});
   };
 
-   handleClick() {
-      this.setState(state => ({
-        isToggleOn: !state.isToggleOn
-      }));
-    }
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
 
   retrieveLastLocation = () => {
     return JSON.parse(localStorage.getItem("location"));
@@ -50,7 +49,7 @@ class App extends Component {
           <Search />
           <CurrentWeather currentObservation={this.state.currentObservation} />
           <button onClick={this.handleClick}>
-            {this.state.isToggleOn ? 'HOURLY' : 'WEEKLY'}
+            {this.state.isToggleOn ? "HOURLY" : "WEEKLY"}
           </button>
           <SevenHour hourlyForecast={this.state.hourlyForecast} />
           <Welcome />
@@ -62,7 +61,7 @@ class App extends Component {
           <Search />
           <CurrentWeather currentObservation={this.state.currentObservation} />
           <button onClick={this.handleClick}>
-            {this.state.isToggleOn ? 'HOURLY' : 'WEEKLY'}
+            {this.state.isToggleOn ? "HOURLY" : "WEEKLY"}
           </button>
           <TenDay dailyForecast={this.state.dailyForecast} />
           <Welcome />
