f4cfa761c789baa4b7e4f9a4b11d365900d1f6b6
Chris Chateau <christopherchateau@gmail.com>
2018-09-12 20:27:35 -0600

    Add trie pop and generateSuggestionCities tests

--- src/Test/Search.test.js
+++ src/Test/Search.test.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import Search from "../Components/Search";
+import { Trie } from "@tmcmeans/autocomplete";
+import cityStateList from "../cityStateList";
 
 describe("Search component", () => {
   let wrapper;
+  let mockTrie;
 
   beforeEach(() => {
     wrapper = shallow(<Search />);
     localStorage.clear();
+    mockTrie = new Trie();
+    mockTrie.populate(cityStateList.data);
   });
 
-  it.skip("should be a thing", () => {
+  it("should be a thing", () => {
     expect(wrapper).toBeDefined();
+  });
+
+  it("trie should be populated with city list", () => {
+    expect(mockTrie.wordCount).toEqual(1000)
+  });
+
+  it("", () => {
+    //console.log(wrapper.instance())
+  });
+
+  it("", () => {
+    let suggestions = wrapper.instance().generatedSuggestedCities('den')
+                             .map( data => data.finalWord)
+    
+    expect(suggestions).toEqual([ 'denver, co', 'denton, tx' ]);
   });
 
 });
